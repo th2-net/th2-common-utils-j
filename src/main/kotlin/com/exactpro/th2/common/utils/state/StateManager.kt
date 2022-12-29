204e4efca364f0d8fc2d1b266af0fdb8f89fb0f3
eugene_zheltov <eugene.zheltov@exactprosystems.com>
2022-12-29 16:34:56 +0400

    [TH2-4596] Add bookId parameter to StateManager.load(...)

--- src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
+++ src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
@@ -1,6 +1,7 @@
 package com.exactpro.th2.common.utils.state
 
+@Suppress("unused")
 interface StateManager {
     fun store(rawData: ByteArray, stateSessionAlise: String)
-    fun load(stateSessionAlise: String): ByteArray?
+    fun load(stateSessionAlise: String, bookId: String): ByteArray?
 }
