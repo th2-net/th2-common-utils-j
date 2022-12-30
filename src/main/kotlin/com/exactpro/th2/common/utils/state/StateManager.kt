30e5859a558defad6e96dcb23e32d90d9f735e88
eugene_zheltov <eugene.zheltov@exactprosystems.com>
2022-12-30 13:33:08 +0400

    [TH2-4596] Add from, to, direction parameters to StateManager.load(...)

--- src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
+++ src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
@@ -1,7 +1,10 @@
 package com.exactpro.th2.common.utils.state
 
+import com.exactpro.th2.common.grpc.Direction
+import java.time.Instant
+
 @Suppress("unused")
 interface StateManager {
     fun store(rawData: ByteArray, stateSessionAlise: String)
-    fun load(stateSessionAlise: String, bookId: String): ByteArray?
+    fun load(from: Instant, to: Instant, direction: Direction, stateSessionAlise: String, bookId: String): ByteArray?
 }
