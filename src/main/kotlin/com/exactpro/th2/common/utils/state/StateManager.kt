22a609f3ab5c9028817c8d1078ac09f867810ad4
eugene_zheltov <eugene.zheltov@exactprosystems.com>
2023-01-09 13:17:02 +0400

    [TH2-4596] Change StateManager interface

--- src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
+++ src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
@@ -1,10 +1,7 @@
 package com.exactpro.th2.common.utils.state
 
-import com.exactpro.th2.common.grpc.Direction
-import java.time.Instant
-
 @Suppress("unused")
 interface StateManager {
-    fun store(rawData: ByteArray, stateSessionAlise: String)
-    fun load(from: Instant, to: Instant, direction: Direction, stateSessionAlise: String, bookId: String): ByteArray?
+    fun store(rawData: ByteArray, stateSessionAlias: String, bookId: String)
+    fun load(stateSessionAlias: String, bookId: String): ByteArray?
 }
