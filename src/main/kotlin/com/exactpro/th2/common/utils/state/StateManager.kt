2d1b0a916a6aa9f0eeaa1fe5f375c03770a1fde1
eugene_zheltov <eugene.zheltov@exactprosystems.com>
2023-01-11 17:00:00 +0400

    [TH2-4596] Add parentEventId: EventID param

--- src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
+++ src/main/kotlin/com/exactpro/th2/common/utils/state/StateManager.kt
@@ -1,7 +1,9 @@
 package com.exactpro.th2.common.utils.state
 
+import com.exactpro.th2.common.grpc.EventID
+
 @Suppress("unused")
 interface StateManager {
-    fun store(rawData: ByteArray, stateSessionAlias: String, bookName: String)
-    fun load(stateSessionAlias: String, bookName: String): ByteArray?
+    fun store(parentEventId: EventID, rawData: ByteArray, stateSessionAlias: String, bookName: String)
+    fun load(parentEventId: EventID, stateSessionAlias: String, bookName: String): ByteArray?
 }
