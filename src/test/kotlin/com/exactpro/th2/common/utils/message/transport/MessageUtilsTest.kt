2281b2ca72bd570dbb88f12b993b6c6237468a72
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2023-07-19 11:41:58 +0400

    [TH2-4989] Added copyFields, addFields for FromMapBuilder * added getFieldSoft extension method * added get* methods for * added message method for ParsedMessage

--- src/test/kotlin/com/exactpro/th2/common/utils/message/transport/MessageUtilsTest.kt
+++ src/test/kotlin/com/exactpro/th2/common/utils/message/transport/MessageUtilsTest.kt
@@ -20,11 +20,13 @@ import com.exactpro.th2.common.message.addField
 import com.exactpro.th2.common.schema.message.impl.rabbitmq.transport.Direction
 import com.exactpro.th2.common.schema.message.impl.rabbitmq.transport.EventId
 import com.exactpro.th2.common.schema.message.impl.rabbitmq.transport.ParsedMessage
+import com.exactpro.th2.common.utils.message.FieldNotFoundException
 import com.exactpro.th2.common.utils.message.toTimestamp
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.time.Instant
 import kotlin.random.Random
 
@@ -194,6 +196,84 @@ class MessageUtilsTest {
     }
 
     @Test
+    fun `get filed value`() {
+        val map = mapOf(
+            "null" to null,
+            "list" to listOf("simple", null),
+            "map" to mapOf(
+                "null" to null,
+                "list" to listOf("simple", null),
+            ),
+            "map-list" to listOf(
+                mapOf(
+                    "null" to null,
+                    "list" to listOf("simple", null),
+                ),
+                null
+            )
+        )
+
+        assertNull(map.getField("fake"))
+        assertNull(map.getField("null"))
+        assertNull(map.getField("list", "1"))
+        assertNull(map.getField("map", "fake"))
+        assertNull(map.getField("map", "null"))
+        assertNull(map.getField("map-list", "1"))
+        assertNull(map.getField("map-list", "0", "null"))
+        assertNull(map.getField("map-list", "0", "list", "1"))
+
+        assertEquals(map["list"], map.getField("list"))
+        assertEquals(map["map-list"], map.getField("map-list"))
+        assertEquals((map["map"] as Map<*, *>)["list"], map.getField("map", "list"))
+        assertEquals(((map["map-list"] as List<*>)[0] as Map<*, *>)["list"], map.getField("map-list", "0", "list"))
+
+        assertThrows<FieldNotFoundException> { map.getField("fake", "fake") }
+        assertThrows<FieldNotFoundException> { map.getField("list", "3") }
+        assertThrows<FieldNotFoundException> { map.getField("map", "fake", "fake") }
+        assertThrows<FieldNotFoundException> { map.getField("map-list", "3") }
+        assertThrows<FieldNotFoundException> { map.getField("map-list", "0", "fake", "fake") }
+    }
+
+    @Test
+    fun `get filed value soft`() {
+        val map = mapOf(
+            "null" to null,
+            "list" to listOf("simple", null),
+            "map" to mapOf(
+                "null" to null,
+                "list" to listOf("simple", null),
+            ),
+            "map-list" to listOf(
+                mapOf(
+                    "null" to null,
+                    "list" to listOf("simple", null),
+                ),
+                null
+            )
+        )
+
+        assertNull(map.getFieldSoft("fake"))
+        assertNull(map.getFieldSoft("null"))
+        assertNull(map.getFieldSoft("list", "1"))
+        assertNull(map.getFieldSoft("map", "fake"))
+        assertNull(map.getFieldSoft("map", "null"))
+        assertNull(map.getFieldSoft("map-list", "1"))
+        assertNull(map.getFieldSoft("map-list", "0", "null"))
+        assertNull(map.getFieldSoft("map-list", "0", "list", "1"))
+
+        assertEquals(map["list"], map.getFieldSoft("list"))
+        assertEquals(map["map-list"], map.getFieldSoft("map-list"))
+        assertEquals((map["map"] as Map<*, *>)["list"], map.getFieldSoft("map", "list"))
+        assertEquals(((map["map-list"] as List<*>)[0] as Map<*, *>)["list"], map.getFieldSoft("map-list", "0", "list"))
+
+        assertNull(map.getFieldSoft("fake", "fake"))
+        assertNull(map.getFieldSoft("list", "3"))
+        assertNull(map.getFieldSoft("map", "fake", "fake"))
+        assertNull(map.getFieldSoft("map-list", "3"))
+        assertNull(map.getFieldSoft("map-list", "0", "fake", "fake"))
+    }
+
+    @Test
     fun `get list value`() {
         val map = mapOf(
             "null" to null,
@@ -250,6 +330,109 @@ class MessageUtilsTest {
 
         assertEquals(map["map"], map.getMap("map"))
         assertEquals((map["map-list"] as List<*>)[0], map.getMap("map-list", "0"))
+    }
+
+    @Test
+    fun `add fields`() {
+        val source = mapOf(
+            "null" to null,
+            "simple" to "test-simple",
+            "list" to listOf("simple", null),
+            "map" to mapOf(
+                "null" to null,
+                "simple" to "test-sub-simple",
+                "list" to listOf("simple", null),
+                "map" to mapOf(
+                    "null" to null,
+                    "simple" to "test-sub-sub-simple",
+                )
+            )
+        )
+        val message = message(MESSAGE_TYPE) {
+            addFields(*source.toList().toTypedArray())
+        }.build()
+
+        assertEquals(source, message.body)
+    }
+
+    @Test
+    fun `copy all fields`() {
+        val source = message(MESSAGE_TYPE) {
+            bodyBuilder().apply {
+                put("null", null)
+                put("simple", "test-simple")
+                put("list", listOf("simple", null))
+                put(
+                    "map", mapOf(
+                        "null" to null,
+                        "simple" to "test-sub-simple",
+                        "list" to listOf("simple", null),
+                        "map" to mapOf(
+                            "null" to null,
+                            "simple" to "test-sub-sub-simple",
+                        )
+                    )
+                )
+            }
+        }.build()
+
+        val target = message(source.type)
+            .copyFields(source, *source.body.keys.toTypedArray())
+            .build()
+
+        assertEquals(source, target)
+    }
+
+    @Test
+    fun `copy sub set of fields`() {
+        val source = message(MESSAGE_TYPE) {
+            addFields(
+                "null" to null,
+                "simple" to "test-simple",
+                "list" to listOf("simple", null),
+                "map" to mapOf(
+                    "null" to null,
+                    "simple" to "test-sub-simple",
+                    "list" to listOf("simple", null),
+                    "map" to mapOf(
+                        "null" to null,
+                        "simple" to "test-sub-sub-simple",
+                    )
+                )
+            )
+        }.build()
+
+        val fieldsSubSet = setOf(source.body.keys.random(), source.body.keys.random())
+        val target = message(source.type)
+            .copyFields(source, *fieldsSubSet.toTypedArray())
+            .build()
+
+        assertEquals(fieldsSubSet, target.body.keys)
+        fieldsSubSet.forEach { field ->
+            assertEquals(source.body[field], target.body[field], "Compare '$field' field value")
+        }
+    }
+
+    @Test
+    fun `get int`() {
+        val source = message(MESSAGE_TYPE) {
+            addFields(
+                "null" to null,
+                "intString" to "123",
+                "int" to 123,
+                "floatString" to "1.23",
+                "float" to 1.23,
+                "string" to "str"
+            )
+        }.build()
+
+        assertNull(source.body.getInt("null"))
+        assertEquals(123, source.body.getInt("intString"))
+        assertEquals(123, source.body.getInt("int"))
+        assertThrows<NumberFormatException>("Get int from 'floatString' filed") { source.body.getInt("floatString") }
+        assertEquals(1, source.body.getInt("float"))
+        assertThrows<NumberFormatException>("Get int from 'string' filed") { source.body.getInt("string") }
+
     }
 
     companion object {
